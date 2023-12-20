fbedf8e0a7cb81a63ad860b9cb9d504133ebb587
luckylukezzz <50476499+luckylukezzz@users.noreply.github.com>
2023-12-20 23:05:24 +0530

    login done react

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,23 +1,24 @@
 import axios from 'axios';
 import {SyntheticEvent, useState} from 'react';
 import { UserErrors } from '../../errors';
+import {useCookies} from 'react-cookie';
+import {useNavigate} from "react-router-dom";
 
 const AuthPage = () => {
     return ( <div>
         <Register />
-        
+        <Login />
     </div> );
 }
  
 export default AuthPage;
 
 const Register = () => {
-    const [username , setUsername] = useState("");
-    const [password , setPassword] = useState("");
+    const [username , setUsername] = useState<string>("");
+    const [password , setPassword] = useState<string>("");
 
     const handleSubmit = async (event: SyntheticEvent) => {
         event.preventDefault();
-        console.log(username,password);
         try{
             await axios.post("http://localhost:3001/user/register",{
                 username,
@@ -56,6 +57,58 @@ const Register = () => {
 };
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 const Login = () => {
-    return ( <div> login</div>);
+    const [username , setUsername] = useState<string>("");
+    const [password , setPassword] = useState<string>("");
+    const [_, setCookies] = useCookies(["access_token"]);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event: SyntheticEvent) => {
+        event.preventDefault();
+        try{
+            const result = await axios.post("http://localhost:3001/user/login",{
+                username,
+                password
+            });
+            setCookies("access_token" , result.data.token);
+            localStorage.setItem("userID", result.data.userID);
+            navigate("/");
+
+        }catch(err){
+            
+            let errorMessage: string = "";
+            switch (err.response.data.type) {
+                case UserErrors.USERNAME_ALREADY_EXISTS:
+                    errorMessage = "User already exists";
+                    break;
+                case UserErrors.WRONG_CREDENTIALS:
+                    errorMessage = "Wrong username/password combination";
+                    break;
+                default:
+                    errorMessage = "Something went wrong";
+            }
+            alert("ERROR: " + errorMessage);
+        }
+    }
+    return ( 
+    <div className="auth-container">
+        <form onSubmit={handleSubmit}>
+            <h2>login</h2>
+            <div className="form-Group">
+                <label htmlFor="username">Username :</label>
+                <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)}  />
+            </div>
+            <div className="form-Group">
+                <label htmlFor="password">Password :</label>
+                <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+            </div>
+            <button type="submit">
+                Login
+            </button>
+        </form>
+    </div>);
 };
+
